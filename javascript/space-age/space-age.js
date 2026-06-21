f63dabb98f247a20be46e86c471349c9b4979957
LmtDNE <dion.fulwood@gmail.com>
2026-06-20 22:37:04 -0700

    feat: Implement solution for space age.

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,29 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const EARTH_YEAR_SECONDS = 31_557_600;
+
+const ORBITAL_PERIODS = {
+	mercury: 0.2408467,
+	venus: 0.61519726,
+	earth: 1.0,
+	mars: 1.8808158,
+	jupiter: 11.862615,
+	saturn: 29.447498,
+	uranus: 84.016846,
+	neptune: 164.79132,
+};
+
+export const age = (planet, seconds) => {
+	const orbitalPeriod = ORBITAL_PERIODS[planet.toLowerCase()];
+
+	if (!orbitalPeriod) {
+		throw new Error('not a planet');
+	}
+
+	const planetAge = seconds / (EARTH_YEAR_SECONDS * orbitalPeriod);
+
+	return Math.round(planetAge * 100) / 100;
+};
