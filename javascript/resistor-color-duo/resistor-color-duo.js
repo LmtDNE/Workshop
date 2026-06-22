51622d0643b5569c017ea2c6e3af22d6d261b549
LmtDNE <dion.fulwood@gmail.com>
2026-06-21 21:36:51 -0700

    style: cleanup

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,28 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
+const COLOR_VALUES = new Map([
+	['black', 0],
+	['brown', 1],
+	['red', 2],
+	['orange', 3],
+	['yellow', 4],
+	['green', 5],
+	['blue', 6],
+	['violet', 7],
+	['grey', 8],
+	['white', 9],
+]);
 export const decodedValue = (resistorColor) => {
-	const resistorMap = new Map([
-		['black', '0'],
-		['brown', '1'],
-		['red', '2'],
-		['orange', '3'],
-		['yellow', '4'],
-		['green', '5'],
-		['blue', '6'],
-		['violet', '7'],
-		['grey', '8'],
-		['white', '9'],
-	]);
-
-	return parseInt(
-		resistorColor
-			.reduce((colorAccum, color, idx) => {
-				if (idx < 2) {
-					colorAccum.push(resistorMap.get(color));
-				}
-				return colorAccum;
-			}, [])
-			.join(''),
-	);
+	return resistorColor
+		.reduce((colorAccum, color, idx) => {
+			if (idx < 2) {
+				colorAccum.push(COLOR_VALUES.get(color));
+			}
+			return colorAccum;
+		}, [])
+		.join('');
 };
